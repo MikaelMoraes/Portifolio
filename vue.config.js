d3d996226e2979ad8a65d81da7d8f841171a2965
Mikael Vitot <68564996+MikaelMoraes@users.noreply.github.com>
2023-03-02 11:02:34 -0300

    teste2

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://portifolio-layout.vercel.app' : '/',
-  transpileDependencies: true,
-  lintOnSave: false
+  publicPath: process.env.NODE_ENV === 'production' ? 'https://portifolio-layout.vercel.appgi' : '/'
+
  
 })
